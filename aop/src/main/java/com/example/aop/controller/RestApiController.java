0db0cb28f2d29acc409617ae305230fc276d19d5
Jaeyah <seongjae9611@daum.net>
2021-05-29 09:15:06 +0900

    ~5/29

---
+++ aop/src/main/java/com/example/aop/controller/RestApiController.java
@@ -0,0 +1,43 @@
+package com.example.aop.controller;
+
+
+import com.example.aop.annotation.Decode;
+import com.example.aop.annotation.Timer;
+import com.example.aop.dto.User;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class RestApiController {
+
+    @GetMapping("/get{id}")
+    public String get(@PathVariable Long id, @RequestParam String name) {
+//        System.out.println("get method");
+//        System.out.println("get method : " + id);
+//        System.out.println("get method : " + name);
+        return id + " " + name;
+    }
+
+    @PostMapping("/post")
+    public User post(@RequestBody User user) {
+//        System.out.println("post method : " + user);
+        return user;
+    }
+
+    @Timer
+    @DeleteMapping("/delete")
+    public void delete() throws InterruptedException{
+
+        // db logic
+        Thread.sleep(1000*2);
+    }
+
+    @Decode
+    @PutMapping("/put")
+    public User put(@RequestBody User user) {
+        System.out.println("put");
+        System.out.println(user);
+        return user;
+    }
+}
---
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,63 @@
+package com.example.client.service;
+
+import com.example.client.dto.UserRequest;
+import com.example.client.dto.UserResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+    // http://localhost/api/server/hello
+    // response
+    public UserResponse hello() {
+        URI uri = UriComponentsBuilder
+            .fromUriString("http://localhost:9090")
+            .path("/api/server/hello")
+            .queryParam("name","aaaa")
+            .queryParam("age",99)
+            .encode()
+            .build()
+            .toUri();
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+
+    public void post() {
+        // http://localhost:9090/api/server/user{userId}/name/{userName}
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+        System.out.println(uri);
+
+        // http body -> ojbect -> object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(uri, req, String.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        // return response.getBody();
+    }
+
+}
